b5eba3dad82ab2a5a8be234a3470618b7f64f544
Авдеев Матвей Анатольевич <maavdeev@edu.hse.ru>
2026-01-07 20:34:50 +0300

    fixes in null strings

--- migrations/001_create_users_table.up.sql
+++ migrations/001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE CHECK (
-        username != "" AND LENGTH(username) BETWEEN 5 AND 25
+        username != '' AND LENGTH(username) BETWEEN 5 AND 25
     ),
     password BLOB NOT NULL CHECK (LENGTH(password) = 64),
     created_at TEXT NOT NULL DEFAULT (DATETIME('now')),
